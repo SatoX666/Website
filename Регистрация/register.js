28653c8389247913b650c7c37467393a83e16210
SatoX666 <99364954+SatoX666@users.noreply.github.com>
2022-10-14 11:50:46 +0300

    reg 2

--- Регистрация/register.js
+++ Регистрация/register.js
@@ -4,15 +4,20 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
-$(".next").click(function(){
+function * profileIter(values) {
+    for (let nextIndex = 0; nextIndex < values.length; ++nextIndex){
+        yield values[nextIndex];
+    }
+}
+(".next").click(function(){
 	if(animating) return false;
 	animating = true;
 	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
+	current_fs = (this).parent();
+	next_fs = (this).parent().next();
 	
 	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	("#progressbar li").eq(("fieldset").index(next_fs)).addClass("active");
 	
 	//show the next fieldset
 	next_fs.show(); 
@@ -42,15 +47,15 @@ $(".next").click(function(){
 	});
 });
 
-$(".previous").click(function(){
+(".previous").click(function(){
 	if(animating) return false;
 	animating = true;
 	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
+	current_fs = (this).parent();
+	previous_fs = (this).parent().prev();
 	
 	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	("#progressbar li").eq(("fieldset").index(current_fs)).removeClass("active");
 	
 	//show the previous fieldset
 	previous_fs.show(); 
@@ -77,6 +82,6 @@ $(".previous").click(function(){
 	});
 });
 
-$(".submit").click(function(){
+(".submit").click(function(){
 	return false;
 })
