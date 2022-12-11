ddef72d8053be9ca0d189e83d2abe3628ebf600d
SatoX666 <petrgile@mail.ru>
2022-12-12 00:08:22 +0300

    Final

---
+++ Тайтл/main.js
@@ -0,0 +1,13 @@
+// scroll
+const start = () => window.scrollTo(0, 0)
+	setTimeout(start, 100)
+
+//Link_alert
+link_alert.onclick = function() {
+    alert("Вы собираетесь перейти на сторонний сайт!")
+}
+
+//logo
+logo.onclick = function() {
+    window.scrollTo(0, 0)
+}
