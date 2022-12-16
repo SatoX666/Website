1401c7dd27202adea4651111f2ab5c5b957a6e84
SatoX666 <petrgile@mail.ru>
2022-12-16 22:16:25 +0300

    123

---
+++ Тайтл/most.php
@@ -0,0 +1,34 @@
+<?php
+// Метод POST
+$name=$_POST ['name1'];
+$surname=$_POST ['surname'];
+$email=$_POST ['email'];
+$pass=$_POST ['password1'];
+
+// Метод GET
+// $name=$_GET ['name1'];
+// $surname=$_GET ['surname'];
+// $email=$_GET ['email'];
+// $pass=$_GET ['password1'];
+
+echo "Успешно";
+
+$link = new mysqli ( 
+    'localhost',        /* Хост, к которому мы подключаемся */ 
+    'root',             /* Имя пользователя */ 
+    '',                 /* Используемый пароль */ 
+    'Shoow');           /* База данных для запросов по умолчанию */ 
+
+if (!$link) { 
+printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error()); 
+exit; 
+}
+
+$sql = $link->prepare ("INSERT INTO Users (Email, Password, Name, Surname) VALUES (?, ?, ?, ?)");
+$sql->bind_param('ssss', $email, $pass, $name, $surname);
+$sql->execute();
+printf("%d Row inserted.\n", $sql->affected_rows);
+$sql->close();
+?>
+
+<script>location.href="index.html"</script>
